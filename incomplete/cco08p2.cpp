ed57f74d6276113d31aa5ad901f22767fc878010
nexus_x86 <60083031+nexus-x86@users.noreply.github.com>
2022-10-16 20:53:39 -0400

    Tried to use 2 disjoint sets and not working

--- incomplete/cco08p2.cpp
+++ incomplete/cco08p2.cpp
@@ -3,84 +3,141 @@
 // Saturday October 15 2022
 
 #include <iostream>
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
-vector<int> p, height, setSize;
 
-int numSets;
-
-void initialize(int N)
-{
-    p.assign(N, 0);
-    for (int i = 0; i < N; ++i)
-        p[i] = i;
-    height.assign(N, 0);  // optional speedup
-    setSize.assign(N, 1); // optional feature
-    numSets = N;          // optional feature
-}
-int findSet(int i)
-{
-    if (p[i] == i)
-        return i;
-    p[i] = findSet(p[i]); // Path Compression
-    return p[i];
-}
-bool isSameSet(int i, int j)
-{
-    return findSet(i) == findSet(j);
-}
-
-int numDisjointSets() // optional
-{
-    return numSets;
-}
-int sizeOfSet(int i) // optional
-{
-    return setSize[findSet(i)];
-}
-
-void unionSet(int i, int j) {
-    if (isSameSet(i, j)) // i and j are in same set
-        return;
-    int x = findSet(i); // find rep items
-    int y = findSet(j); // find rep items
-    if (height[x] > height[y])
-        swap(x, y); // Union by rank
-    p[x] = y;       // set x under y
-    if (height[x] == height[y])
-        ++height[y];          // optional speedup
-    setSize[y] += setSize[x]; // combine set sizes at y
-    --numSets;                // a union reduces numSets
-}
+int M,N;
+int count = 1;
 
 const int mx = 1e5+5;
 
-unordered_map<string, int> streets;
-int runningCounter = 1;
+unordered_map<string, int> snum;
+vector<int> parent, size1, parent2, size2;
 
-// all the parallel lines can be compressed to a single line
-// carefully note that is a and b are parallel and c and d are parallel this does not mean d is parallel to a
-// it can be assumed that if a line intersects 1 parallel line it intersects with all of them
+void make_set(int v, int s) {
+    if (s == 1) {
+        parent[v] = v;
+        size1[v] = 1;
+    } else {
+        parent2[v] = v;
+        size2[v] = 1;
+    }
+}
+
+int find_set(int v, int s) {
+    if (s == 1) {
+        if (v == parent[v]) {
+            return v;
+        }
+        parent[v] = find_set(parent[v],1);
+        return parent[v];
+    } else {
+        if (v == parent2[v]) {
+            return v;
+        }
+        parent2[v] = find_set(parent2[v],2);
+        return parent2[v];
+    }
+}
+// f(1) = 2
+// f(2) = 1
+
+bool isSameGroupSet(int i, int j, int s) {
+    int a = find_set(i,s); // root of i in set s
+    int b = find_set(j,s); // root of j in set s
+    if (s == 2) {
+        s = 1;
+    } else {
+        s = 2;
+    }
+    int c = find_set(i,s); // root of i in set not s
+    int d = find_set(j,s); // root of j in set not s
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    cout << d << "\n";
+    return a == d || a == b || b == c || c == d;
+}
+
+bool isSameSet(int i, int j, int s)
+{
+    return find_set(i, s) == find_set(j, s);
+}
+
+void union_sets(int a, int b, int s) {
+    if (s == 1) {
+        a = find_set(a,1);
+        b = find_set(b,1);
+        if (a != b) {
+            if (size1[a] < size1[b]) {
+                swap(a,b);
+            }
+            parent[b] = a;
+            size1[a] += size1[b];
+        }
+    } else {
+        a = find_set(a,2);
+        b = find_set(b,2);
+        if (a != b) {
+            if (size2[a] < size2[b]) {
+                swap(a,b);
+            }
+            parent2[b] = a;
+            size2[a] += size2[b];
+        }
+    }
+}
+
+// parent1 -> parallel relationships
+// parent2 -> intersect relationships
+
 int main() {
-    initialize(mx);
-    int m, n;
-    cin >> m >> n;
-    for (int i = 0; i < m; i++) {
-        string a,b;
+    parent.assign(mx, 0);
+    size1.assign(mx,0);
+    parent2.assign(mx,0);
+    for (int i = 1; i <= mx; i++) {
+        parent[i] = i;
+        parent2[i] = i;
+    }
+    size2.assign(mx,0);
+    cin >> M >> N;
+    for (int i = 0; i < M; i++) {
+        string a, b, c;
+        cin >> a >> b >> c;
+        int q = 3;
+        if (c == "parallel") {
+            q = 1;
+        } else {
+            q = 2;
+        }
+        if (snum[a] == 0) {
+            snum[a] = count;
+            count++;
+            make_set(snum[a],q);
+        }
+        if (snum[b] == 0) {
+            snum[b] = count;
+            count++;
+            make_set(snum[b],q);
+        }
+        union_sets(snum[a],snum[b],q);
+        if (isSameSet(snum[a],snum[b],1) == true && isSameSet(snum[a],snum[b],2) == true) {
+            cout << "Waterloo" << "\n";
+            return 0;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        string a, b;
         cin >> a >> b;
-        if (streets[a] == 0) {
-            streets[a] = runningCounter;
-            runningCounter++; 
+        if (isSameGroupSet(snum[a],snum[b],1)) {
+            cout << "parallel" << "\n";
+        } else if (isSameGroupSet(snum[a],snum[b],2)) {
+            cout << "intersect" << "\n";
+        } else {
+            cout << "unknown" << "\n";
         }
-        if (streets[b] == 0) {
-            streets[b] = runningCounter;
-            runningCounter++; 
-        }
-        string c;
-        cin >> c;
-        
     }
 }
