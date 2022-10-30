a2619a43106f8def68ba69c6993853b9b7b9c9b5
nexus_x86 <60083031+nexus-x86@users.noreply.github.com>
2022-10-30 18:55:05 -0400

    Working on this right now

---
+++ incomplete/rgpc18p3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, T;
+    cin >> N >> T;
+    // N Cups from 1 to N filled up T times
+    // for each filling, c chocolates are added to all cups between cup a and b inclusive
+
+    // special someone can be given more than 1 cup, and the cups must be numbered in order
+    // special someone can be given a max of L chocolates
+}
