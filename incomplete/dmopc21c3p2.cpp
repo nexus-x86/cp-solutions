970830b1d7dc737f7b320a5454305139cdd5c29b
nexus_x86 <60083031+nexus-x86@users.noreply.github.com>
2022-11-01 16:48:19 -0400

    Working on this

---
+++ incomplete/dmopc21c3p2.cpp
@@ -0,0 +1,23 @@
+// https://dmoj.ca/problem/dmopc21c3p2
+// https://github.com/nexus-x86/cp-solutions
+// Monday October 31 2022 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int K;
+    cin >> K;
+
+    vector<int> A; // length N
+
+    // Given an integer array A of length N
+    // (1 <= N <= 1e6)
+    // where elements are [-1e9,1e9]
+    // output the number of non-empty contiguous subarrays with an even sum
+    
+    // we want to make test data for this problem
+    // find an array of length n with exactly K non-empty contiguous subarrays with an even sum
+    
+}
