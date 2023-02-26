933dd9861d5d7ce157855ccf6d2ae514b595a4cf
nexus_x86 <60083031+nexus-x86@users.noreply.github.com>
2023-02-26 14:38:32 -0500

    cleaning up notes and making them better

--- notes/tricks/printf.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    // printf is superior to cout in terms of formatting
-    printf("%08d\n", 42); // integer with 8 characters in width
-    printf("%.2f\n", 42.2); // float with 2 places after decimal points
-}
